3cd289156fd715bbae370a5848b5ee8679360b3e
Johannes <johannes-programming@posteo.org>
2023-11-16 22:29:21 +0100

    Version 0.0.1

--- src/handle_igblastn/__init__.py
+++ src/handle_igblastn/__init__.py
@@ -1,5 +1,2 @@
-def main(args=None):
-    print("Hello World!")
-    
-if __name__ == '__main__':
-    main() 
+from ._parsers import *
+from ._clines import *
--- src/handle_igblastn/__main__.py
+++
@@ -1,4 +0,0 @@
-from handle_igblastn import main
-
-if __name__ == '__main__':
-    main()
